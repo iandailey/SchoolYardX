18469f38a1397327e545e1d0920a68841fa526e3
iandailey <150456725+iandailey@users.noreply.github.com>
2024-04-23 14:50:49 -0400

    Create user.php

---
+++ user.php
@@ -0,0 +1,80 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>SchoolYard Exchange</title>
+    <link rel="stylesheet" href="home-layout.css">
+
+  </head>
+  <body>
+    <header class="topnav">
+      <a href="home.html" id="mainpage">SchoolYard Exchange</a>
+      <input type="text" placeholder="Search the SchoolYard" id="searchbar" />
+      <a href="login.html" id="loginlink">Login</a>
+      <a href="favorites.html" id="favlink">Favorites</a>
+      <a href="dashboard.html" id="dashlink">Dashboard</a>
+    </header>
+
+    <main>
+      <?php
+  
+      if(isset($_SESSION['Email'])) {
+            $fname = $_SESSION['fname'];
+            echo "<h2> Welcome $fname! </h2>";
+          
+        }
+
+        ?>
+      <br><br>
+      <button onclick="window.location.href = 'additem.php';">Add a new item!</button>
+      <br><br>
+      <h4>Edit Posted Items</h4>
+      <?php
+      if(isset($_SESSION['userid'])){
+        $userid = $_SESSION['userid'];
+        include 'dbconnect.php';
+
+        $sql = "SELECT * FROM Items where UserID = '$userid'";
+
+        $result = $conn->query($sql);
+
+        if ($result->num_rows>0) {
+          echo "<table>";
+          echo "<thead>
+          <tr>
+            <th>Item</th>
+            <th>Category</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>";
+          while($row = $result->fetch_assoc()){
+            echo "<tr>";
+            echo "<td>" . $row['prod_name'] . "</td>";
+            echo "<td>" . $row['Category'] . "</td>";
+            echo "<td>" . $row['SoldStatus'] . "</td>";
+            echo "<td><button class='delete-btn' data-id='{" . $row['ListingID'] . "}'>Delete</button></td>";
+            echo "</tr>";
+          }
+          echo "</tbody>";
+          echo "</table>";
+        } else {
+          echo "You have not made any posts yet.";
+        }
+      
+        $conn->close();
+      }
+   
+
+
+
+      ?>
+      <script src="userjs.js"></script>
+        
+  </main>
+  </body>
+</html>
